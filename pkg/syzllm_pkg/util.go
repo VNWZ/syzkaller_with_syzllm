26ad569ccda1d3f349434593326fe84396f95b9c
AmoyCherry <840014803@qq.com>
2025-06-07 11:48:42 -0400

    fix syzllm prob to 0.35

--- pkg/syzllm_pkg/util.go
+++ pkg/syzllm_pkg/util.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-var SyzllmProbabilityFuzzer = 0.3
+var SyzllmProbabilityFuzzer = 0.35
 var MutationSelectionRand = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 type SyscallRequestData struct {
